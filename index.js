bb35079eec6fafd3a275f7f2252b4bbddef2ac9b
John Ottenlips <jottenlips@gmail.com>
2017-02-03 21:04:22 -0600

    new appid and masterkey

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || '00100100',
+  masterKey: process.env.MASTER_KEY || '55ba92cc-f659-4a2b-adb4-ae2168447a28', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:3000/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
