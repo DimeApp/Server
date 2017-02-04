527aefcdca3441d01eafd639ea3adc35d603674e
John Ottenlips <jottenlips@gmail.com>
2017-02-04 00:10:31 -0600

    plaid api request

--- cloud/main.js
+++ cloud/main.js
@@ -52,16 +52,39 @@ Parse.Cloud.define('removeCharity', function(request, response){
 
 Parse.Cloud.define('getTransactions', function(request,response){
   const user = request.user;
+
   if(user == null){
     response.error('Either no session token or session token has expired');
   }
+  const bankUserName = request.params.username;
+  const password = request.params.password;
+  const bank = request.params.bank;
+  // const pin = request.parans.pin;
 
+  const client_id = process.env.client_id
+  const secret = process.env.secret
+  // process.env.
+  fetch("https://tartan.plaid.com/info", {
+    method: 'POST',
+    body: JSON.stringify({
+      "client_id":client_id,
+      "secret":secret,
+      "username":bankUserName,
+      "password":password,
+      "type":bank
+    })
 
+  }).then(function(resp){
+    response.success(resp);
+  }, function(error){
+    console.error(error);
+    response.error(error);
+  })
 });
 
 
 
-/ fetch(parse_api_root + 'functions/addCharity', {
+// fetch(parse_api_root + 'functions/addCharity', {
 //   method: 'POST',
 //   headers: {
 //     'X-Parse-Application-Id': '001001',
