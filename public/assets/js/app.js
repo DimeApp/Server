86d5342ce9ede449c30bee98fc8ae20374745506
justinbavier <jbavier123@gmail.com>
2017-02-20 09:22:37 -0600

    Add React

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,15 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, IndexRoute, browswerHistory} from 'react-router'
 
-class Home extends React.Componenet {
-  render() {
-    return (
-      <h1>Dime</h1>
-    );
-  }
-}
 
 ReactDOM.render(
-  <Home />,
+  <h1>Hello, world!</h1>,
   document.getElementById('app')
 );
