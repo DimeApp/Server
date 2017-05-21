198b0e414fcd5106907e42d3a9a5867c825d5508
John Ottenlips <jottenlips@gmail.com>
2017-05-21 14:08:40 -0500

    efe

--- cloud/main.js
+++ cloud/main.js
@@ -182,24 +182,24 @@ Parse.Cloud.define('storePlaidPublicToken', function(request, response){
 
 //
 //
-Parse.Cloud.define('stripeToken', function(request,response){
-
-  const user = request.user;
-
-  query.get(user.id).then(function(user){
-    var public_token = user.get('public_token');
-    if (public_token != null) {
-
-      plaidClient.exchangeToken(public_token,
-                              '[Plaid Link account_id]',
-                              function(err, res) {
-      var bankAccountToken = res.stripe_bank_account_token;
-        response.success(res);
-      });
-    });
-
-});
-});
+// Parse.Cloud.define('stripeToken', function(request,response){
+//
+//   const user = request.user;
+//
+//   query.get(user.id).then(function(user){
+//     var public_token = user.get('public_token');
+//     if (public_token != null) {
+//
+//       plaidClient.exchangeToken(public_token,
+//                               '[Plaid Link account_id]',
+//                               function(err, res) {
+//       var bankAccountToken = res.stripe_bank_account_token;
+//         response.success(res);
+//       });
+//     });
+//
+// });
+// });
 //
 
 //
