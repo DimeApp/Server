775474de24afbb33817f966bcf1359e9e846d31f
John Ottenlips <jottenlips@gmail.com>
2017-06-14 19:33:14 -0500

    stripe access token, account dictionary test

--- cloud/main.js
+++ cloud/main.js
@@ -195,7 +195,7 @@ Parse.Cloud.define('stripeToken', function(request,response){
     }).then(plaidClient.getConnectUser(access_token, function(err, res) {
         // response.success(res);
          var accountDictionary = res;
-        //  return accountDictionary;
+        return accountDictionary;
         //  query.get(user.id).then(function(user){
         //    var public_token = user.get('public_token');
         //   //  response.success(public_token);
