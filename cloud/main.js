f88dfa0f01088fdf3aee1803e394fa2f19d00066
justinbavier <jbavier123@gmail.com>
2017-07-18 10:32:14 -0500

    Plaid

--- cloud/main.js
+++ cloud/main.js
@@ -17,7 +17,7 @@ var PLAID_ENV        = envvar.string('PLAID_ENV', 'tartan');
 var client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
-  PLAID_PUBLIC_KEY,
+  //PLAID_PUBLIC_KEY,
   plaid.environments.tartan
 );
 
