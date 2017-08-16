28b0877102dfac129b3a59844c792fa4bbfa5f4b
John Ottenlips <jottenlips@gmail.com>
2017-08-16 18:45:07 -0500

    testing plaid access

--- cloud/main.js
+++ cloud/main.js
@@ -271,7 +271,8 @@ Parse.Cloud.define('storePlaidAccessToken', function(request, response){
     if (error != null) {
       var msg = 'Could not exchange public_token!';
       console.log(msg + '\n' + error);
-      response.console.error(error);
+      // response.console.error(error);
+      response.error('Give me a real token boy');
     }
     ACCESS_TOKEN = tokenResponse.access_token;
     ITEM_ID = tokenResponse.item_id;
